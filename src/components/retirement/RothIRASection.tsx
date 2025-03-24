0dbfd1af58c5f5149d98d171b66afa596b7baf0f
Niamake <stanley.kong.official@gmail.com>
2025-03-24 16:12:05 -0400

    Update RothIRASection.tsx

--- src/components/retirement/RothIRASection.tsx
+++ src/components/retirement/RothIRASection.tsx
@@ -248,7 +248,7 @@ const RothIRASection = ({ savedData, onSave }: RothIRASectionProps) => {
                   ))}
                 </div>
                 
-                <div className="h-[500px] w-full">
+                <div className="h-[400px] w-full">
                   <ChartContainer
                     config={{
                       balance: {
